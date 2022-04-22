a472aea9356abec13da1fe0ad12f7d0a6086bd94
kevd82 <80932709+kevd82@users.noreply.github.com>
2022-04-21 22:05:11 -0700

    Add files via upload

---
+++ Fronts.js
@@ -0,0 +1,39 @@
+class SLLNode { 
+    constructor(val) {
+        this.value = val; 
+        this.next = null; 
+    }
+}
+
+class SLL { 
+    constructor() { 
+        this.head = null;
+    }
+
+
+
+addFront(value) {
+    var newNode = new SLLNode(value); 
+    newNode.next = this.head; 
+    this.head = newNode; 
+}
+
+removeFront() {
+    if (this.head == null) {
+        return this.head;
+    }
+    var removedNode = this.head;
+    this.head = removedNode.next;
+    removedNode.next = null;
+    return this.head;
+}
+
+front() {
+    if (this.head == null) {
+        return null;
+    } else { 
+        return this.head.value;
+    }
+
+}
+}
